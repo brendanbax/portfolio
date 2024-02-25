0544b2a0fbf46ef8f8d7c56e494a21f596e8e3c2
brendanbax <brendanbax@gmail.com>
2024-02-24 21:26:28 -0600

    add css vars to theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			backgroundImage: {
 				topography: "url('./topography.svg')"
+			},
+			colors: {
+				primary: 'var(--color-primary)',
+				secondary: 'var(--color-secondary)',
+				accent: 'var(--color-accent)',
+				default: 'var(--color-text-default)',
+				muted: 'var(--color-text-muted)'
+			},
+			fontFamily: {
+				sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+				serif: ['var(--font-serif)', ...defaultTheme.fontFamily.serif],
+				heading: ['var(--font-heading)', ...defaultTheme.fontFamily.sans]
 			}
 		}
 	},
